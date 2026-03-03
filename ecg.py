62622db8b310e5b664f838efdb7632f1fdced329
Emil Motrøen <e.motroen@gmail.com>
2026-03-03 14:36:30 +0100

    Refactor ECG to its own class

---
+++ ecg.py
@@ -0,0 +1,260 @@
+import numpy as np
+import neurokit2 as nk
+import os
+import pandas as pd
+import matplotlib.pyplot as plt
+from enum import Enum
+
+
+class TypeECG(Enum):
+    ORIGINAL = 1
+    RECONSTRUCTED = 2
+
+
+SAMPLING_RATE = 500  # Data is retrieved from records500
+LEAD_LABELS = [
+    'I', 'II', 'III',
+    'aVR', 'aVL', 'aVF',
+    'V1', 'V2', 'V3',
+    'V4', 'V5', 'V6',
+]
+TOTAL_NUM_LEADS = len(LEAD_LABELS)
+TOTAL_NUM_ECGS = 0
+
+
+class ECG:
+    df_r_peak_mean_data = pd.DataFrame({'Original': [], 'Reconstructed': []})
+
+    def __init__(self, input_data, type_ecg: TypeECG):
+        self.type_ecg = type_ecg
+        self.data = self.load_data(input_data)
+        self.ecg_r_peaks = {}
+
+    def get_r_peaks_empty(self):
+        """
+        Check if the ecg_r_peaks dictionary is empty since certain operations requires it to be not empty.
+        :return: True if empty, False otherwise.
+        """
+        if not self.ecg_r_peaks:
+            return True
+        return False
+
+    def get_r_peaks(self, ecg_number: int, lead_number: int, r_value: int=None):
+        """
+        Get the R-peaks for an ECG and lead signal.
+        :param ecg_number: The ECG number.
+        :param lead_number: The lead number.
+        :param r_value: Used to get a specific R-peak value from an ECG and lead signal.
+        :return: The R-peaks for an ECG and lead signal.
+        """
+        self.check_boundaries(ecg_number, lead_number)
+        if r_value is not None:
+            return self.ecg_r_peaks[(ecg_number, slice(None, None, None), lead_number)][r_value]
+        # TODO: Maybe redefine the keys for the R-peaks to drop the slice() bit
+        return self.ecg_r_peaks[(ecg_number, slice(None, None, None), lead_number)]
+
+    def get_shape(self):
+        return self.data.shape
+
+    def __eq__(self, other):
+        """
+        Compare the shape of the data as both the original and reconstructed data should have the same shape.
+        :param other: The other ECG object.
+        :return:
+        """
+        if not isinstance(other, self.__class__):
+            return NotImplemented
+        return self.get_shape() == other.get_shape()
+
+    def load_data(self, data):
+        """
+        Load the data in the data.npy.npz file.
+        :param data: The npy.npz file after running main.ipynb
+        :return: The original or reconstructed data.
+        """
+        if not os.path.exists(data):
+            raise FileNotFoundError(f'File {data} not found.')
+        with np.load(data) as data:
+            if self.type_ecg == TypeECG.ORIGINAL:
+                v = data['original']
+            else:
+                v = data['reconstructed']
+            global TOTAL_NUM_ECGS
+            TOTAL_NUM_ECGS = len(v)
+        return v
+
+    def find_r_peaks(self, ecg_start=0, ecg_end=TOTAL_NUM_ECGS, lead_start=0, lead_end=TOTAL_NUM_LEADS):
+        """
+        Find R-peaks for a range of ECGs and leads and store the peaks and mean of peaks.
+        :param ecg_start: The start ECG index.
+        :param ecg_end: The end ECG index.
+        :param lead_start: The lead start index.
+        :param lead_end: The lead end index.
+        :return: None
+        """
+        print(f'Calculating mean of R-peaks on ECG(s) [{ecg_start} - {ecg_end - 1}] using '
+              f'lead(s) [{LEAD_LABELS[lead_start]} - {LEAD_LABELS[lead_end - 1]}]')
+        for ecg in range(ecg_start, ecg_end):
+            for lead in range(lead_start, lead_end):
+                peaks = self.get_r_peaks_from_signal(ecg_num=ecg, lead_num=lead)
+                peak_mean = self.calculate_r_peak_mean(peaks, ecg, lead, print_r_peaks=False, print_mean_values=False)
+                if self.type_ecg == TypeECG.ORIGINAL:
+                    ECG.df_r_peak_mean_data.loc[ecg, 'Original'] = peak_mean
+                else:
+                    ECG.df_r_peak_mean_data.loc[ecg, 'Reconstructed'] = peak_mean
+
+    def calculate_r_peak_mean(self, peaks, ecg_num, lead_num, print_mean_values=False, print_r_peaks=False):
+        """
+        Calculate the R-peak mean.
+        :param peaks: The R-peaks.
+        :param ecg_num: The index of the ECG signal.
+        :param lead_num: The lead.
+        :param print_mean_values: Print the mean of the R-peaks of the original and reconstructed signals.
+        :param print_r_peaks: Print the R-peaks of the original and reconstructed signals.
+        :return: The mean of the original and reconstructed signals.
+        """
+        mean = np.mean(peaks)
+        if print_r_peaks:
+            self.print_peaks(peaks)
+        if print_mean_values:
+            print(f'Mean value of original R-peaks for ECG {ecg_num} and lead {LEAD_LABELS[lead_num]}: {mean:.4f}')
+        return mean
+
+    def print_peaks(self, data):
+        """
+        Print the R-peaks from the ECG signal to the terminal.
+        :param data: The ECG signal with R-peaks.
+        :return: None
+        """
+        if self.type_ecg == TypeECG.ORIGINAL:
+            print(f'Original peaks: Total {len(data)} peaks\n{data}')
+        else:
+            print(f'Reconstructed peaks: Total {len(data)} peaks\n{data}')
+
+    def get_r_peaks_from_signal(self, ecg_num, lead_num):
+        """
+        Get the R-peaks from an ECG and lead signal.
+        :param ecg_num: The ECG index.
+        :param lead_num: The lead
+        :return: The R-peaks from the signal.
+        """
+        ecg, signal, r_peaks = self.process_signal(ecg_num, lead_num)
+        self.ecg_r_peaks[ecg_num, :, lead_num] = r_peaks["ECG_R_Peaks"]
+        if self.type_ecg == TypeECG.ORIGINAL:
+            title = f'- Original - ECG {ecg_num} - Lead {LEAD_LABELS[lead_num]}'
+        else:
+            title = f'- Reconstructed - ECG {ecg_num} - Lead {LEAD_LABELS[lead_num]}'
+        peaks = self.plot_r_peaks(ecg, r_peaks,
+                                  title_suffix=title,
+                                  use_plotting=False,
+                                  zoom=False,
+                                  zoom_level=3)
+        return peaks
+
+    @staticmethod
+    def plot_r_peaks(ecg_signal, info, title_suffix='', zoom=False, zoom_level=5, use_plotting=False):
+        """
+        After getting the R-peaks, the R-peaks can be plotted.
+        :param ecg_signal: The ECG signal.
+        :param info: The ECG information.
+        :param title_suffix: The plot title.
+        :param zoom: Zoom in on plots to focus only on a few peaks.
+        :param zoom_level: The zoom level.
+        :param use_plotting: Plot the R-peaks.
+        :return: R-peaks.
+        """
+        rpeaks = info['ECG_R_Peaks']
+        if use_plotting:
+            if zoom:
+                max_idx = int(zoom_level * SAMPLING_RATE)
+                mask = (rpeaks < max_idx)
+                rpeaks_to_plot = rpeaks[mask]
+                ecg_to_plot = ecg_signal[:max_idx]
+            else:
+                rpeaks_to_plot = rpeaks
+                ecg_to_plot = ecg_signal
+            nk.events_plot(rpeaks_to_plot, ecg_to_plot)
+            plt.grid(True, alpha=0.3)
+            plt.title(f'$R$-peaks {title_suffix}')
+            plt.xlabel('Samples')
+            plt.ylabel('Amplitude')
+            plt.tight_layout()
+            plt.show()
+        return rpeaks
+
+    def process_signal(self, ecg_num, lead_num):
+        """
+        Process the ECG signal using NeuroKit's preprocessing pipeline.
+        :param ecg_num: The index of the ECG signal.
+        :param lead_num: The lead.
+        :return: A processed ECG signal, its signal information and the R-peaks.
+        """
+        ecg = self.get_ecg(ecg_num, lead_num)
+        ecg, _ = nk.ecg_invert(ecg, sampling_rate=SAMPLING_RATE)
+        signals, r_peaks = nk.ecg_process(ecg, sampling_rate=SAMPLING_RATE, method='neurokit')
+        return ecg, signals, r_peaks
+
+    def get_ecg(self, ecg_num: int, lead_num: int):
+        """
+        Get and clean a single ECG signal.
+        :param ecg_num: The index of the ECG signal.
+        :param lead_num: The lead to plot.
+        :return: ECG signal.
+
+        Example: To get the first ECG with the first lead of the original data:
+        ::
+            ecg_signal = original.get_ecg(0, 0)
+        """
+        self.check_boundaries(ecg_num, lead_num)
+        ecg = self.data[ecg_num, :, lead_num]
+        cleaned_ecg = nk.ecg_clean(ecg, sampling_rate=SAMPLING_RATE)
+        return cleaned_ecg
+
+    def check_boundaries(self, ecg_num, lead_num) -> None:
+        """
+        Check if the ECG signal has the required boundaries. For example there is only 12 leads
+        so trying to use lead 13 which doesn't exist raises an error.
+        :param ecg_num: The index of the ECG signal.
+        :param lead_num: The lead.
+        :return: None
+        """
+        n, t, c = self.get_shape()  # (N, T, C) -> (4368, 5000, 12)
+        if not 0 <= ecg_num < n:
+            raise IndexError(f'ECG signal index {ecg_num} out of range [0, {n - 1}]')
+        if not 0 <= lead_num < c:
+            raise IndexError(f'Lead number {lead_num} out of range [0, {c - 1}]')
+
+    def find_ecg_peaks(self, ecg_start=0, ecg_end=TOTAL_NUM_ECGS,
+                       lead_start=0, lead_end=TOTAL_NUM_LEADS, use_plotting=False, zoom_level=4):
+        """
+        Find the peaks of the ECG signals.
+        :param ecg_start: The start index of the ECG signal.
+        :param ecg_end:  The end index of the ECG signal.
+        :param lead_start: The start index of the lead.
+        :param lead_end: The end index of the lead.
+        :param use_plotting: Plot the ECG peaks.
+        :param zoom_level: The zoom level for the plotted ECG peaks.
+        :return: None
+        """
+        if self.get_r_peaks_empty():
+            print('Finding the ECG peaks requires having the R-peaks, find the R-peaks before the ECG peaks.')
+            raise SystemExit
+        for ecg in range(ecg_start, ecg_end):
+            for lead in range(lead_start, lead_end):
+                current_ecg = self.get_ecg(ecg_num=ecg, lead_num=lead)
+                r_peaks = self.get_r_peaks(ecg, lead)
+                _, waves_peak = nk.ecg_delineate(current_ecg, r_peaks, sampling_rate=SAMPLING_RATE,
+                                                 method='peak', show=True, show_type='peaks')
+                if use_plotting:
+                    plot = nk.events_plot([waves_peak['ECG_T_Peaks'][:zoom_level],
+                                           waves_peak['ECG_P_Peaks'][:zoom_level],
+                                           waves_peak['ECG_Q_Peaks'][:zoom_level],
+                                           waves_peak['ECG_S_Peaks'][:zoom_level]],
+                                          current_ecg[:(zoom_level + 1) * SAMPLING_RATE])
+                    plt.grid(True, alpha=0.3)
+                    plt.title(f'Peaks - {self.type_ecg.name[0] + self.type_ecg.name[1:].lower()} ECG - '
+                              f'{ecg} - Lead {LEAD_LABELS[lead]}')
+                    plt.xlabel('Samples')
+                    plt.ylabel('Amplitude')
+                    plt.tight_layout()
+                    plt.show()
--- eval.py
+++ eval.py
@@ -1,107 +1,14 @@
-import neurokit2 as nk
-import numpy as np
-import matplotlib.pyplot as plt
-import os
+from ecg import ECG, TypeECG, SAMPLING_RATE, LEAD_LABELS, TOTAL_NUM_ECGS, TOTAL_NUM_LEADS
 
-SAMPLING_RATE = 500 # Data is retrieved from records500
-DATA_SHAPE = (4368, 5000, 12) # Shape of the original and reconstructed data
-LEAD_LABELS = [
-    'I',  'II', 'III',
-    'aVR','aVL','aVF',
-    'V1', 'V2', 'V3',
-    'V4', 'V5', 'V6',
-]
-TOTAL_NUM_LEADS = len(LEAD_LABELS)
-
-def plot_ecg(org_ecg, rec_ecg, ecg_num: int = 0, lead_num: int = 0) -> None:
-    """
-    Plot the original and reconstructed ECG signal using matplotlib.
-    :param org_ecg: The original ECG signal.
-    :param rec_ecg: The reconstructed ECG signal.
-    :param ecg_num: The index of the ECG signal.
-    :param lead_num: The lead to plot.
-    :return: None
-    """
-    plt.figure(figsize=(10, 5))
-    plt.plot(org_ecg[ecg_num, :, lead_num], label='original')
-    plt.plot(rec_ecg[ecg_num, :, lead_num], label='reconstructed', alpha=0.7)
-    plt.title(f'ECG {ecg_num} – Lead {lead_num}')
-    plt.legend()
-    plt.tight_layout()
-    plt.grid()
-    plt.show()
-
-
-def load_data(data):
-    """
-    Load the data.
-    :param data: The npy.npz file after running main.ipynb
-    :return: A tuple of the original and reconstructed ECG signal.
-    """
-    if not os.path.exists(data):
-        raise FileNotFoundError(f'File {data} not found.')
-    with np.load(data) as data:
-        org = data['original']
-        rec = data['reconstructed']
-    return org, rec
-
-
-def check_boundaries(data, ecg_num, lead_num) -> None:
-    """
-    Check if the ECG signal has the required boundaries. For example there is only 12 leads
-    so trying to use lead 13 which doesn't exist is asserted.
-    :param data: The original or reconstructed ECG signal.
-    :param ecg_num: The index of the ECG signal.
-    :param lead_num: The lead.
-    :return: None
-    """
-    assert ecg_num < len(data), f'ecg_num must be less than {len(data)}'
-    assert lead_num < TOTAL_NUM_LEADS, f'lead_num must be less than {TOTAL_NUM_LEADS}'
-
-
-def plot_nk_r_peaks(data, ecg_num: int = 0, lead_num: int = 0, zoom: bool = False, zoom_level: int = 5) -> None:
-    """
-    Plot the R peaks of original or reconstructed ECG signal using neurokit2.
-    :param data: The ECG signal.
-    :param ecg_num: The index of the ECG signal.
-    :param lead_num: The lead to plot.
-    :param zoom: Whether to zoom the plot.
-    :param zoom_level: The zoom level. A zoom level of 5 will only show 5 peaks.
-    :return: None
-    """
-    if data.shape == DATA_SHAPE:
-        ecg_signal = get_ecg(data, ecg_num, lead_num)
-    else:
-        ecg_signal = data
-    _, rpeaks = nk.ecg_peaks(ecg_signal, sampling_rate=SAMPLING_RATE)
-    if zoom:
-        plot = nk.events_plot(rpeaks['ECG_R_Peaks'][:zoom_level], ecg_signal[:zoom_level * SAMPLING_RATE])
-    else:
-        plot = nk.events_plot(rpeaks['ECG_R_Peaks'], ecg_signal)
-    plt.grid()
-    plt.title(f'$R$ peaks of ECG {ecg_num} – Lead {LEAD_LABELS[lead_num]}')
-    plt.xlabel('$x$')
-    plt.ylabel('$y$')
-    plt.show()
-
-
-def get_ecg(data, ecg_num, lead_num):
-    """
-    Get and clean a single ECG signal.
-    :param data: The original or reconstructed ECG signal.
-    :param ecg_num: The index of the ECG signal.
-    :param lead_num: The lead to plot.
-    :return: ECG signal.
-
-    Example: To get the first ECG with the first lead of the original data:
-    ::
-        ecg_signal = get_ecg(original, 0, 0)
-    """
-    check_boundaries(data, ecg_num, lead_num)
-    ecg = data[ecg_num, :, lead_num]
-    cleaned_ecg = nk.ecg_clean(ecg, sampling_rate=SAMPLING_RATE)
-    return cleaned_ecg
 
 if __name__ == '__main__':
-    original, reconstructed = load_data('output/data.npy.npz')
-    plot_nk_r_peaks(original, ecg_num=0, lead_num=0)
+    # TODO: Make a container for the data so we don't have to load it twice
+    original = ECG('output/data.npy.npz', TypeECG.ORIGINAL)
+    reconstructed = ECG('output/data.npy.npz', TypeECG.RECONSTRUCTED)
+    assert original.__eq__(reconstructed), "Original and reconstructed signals do not match."
+
+    ecg_number = 0
+    lead_number = 0
+    TOTAL_NUM_ECGS = 1  # Don't want to go through all ECGs yet
+    original.find_r_peaks(ecg_number, TOTAL_NUM_ECGS, lead_number, lead_number+1)
+    original.find_ecg_peaks(ecg_number, TOTAL_NUM_ECGS, lead_number, lead_number+1, use_plotting=True)
